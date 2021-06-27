331fe9a84457da6d56bf2d1f41e68b8745b735c4
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-27 20:14:20 +0900

    userモデルにバリデーションを設定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :occupation, presence: true
+       
+  has_many :lunches
+  has_many :comments
 end
