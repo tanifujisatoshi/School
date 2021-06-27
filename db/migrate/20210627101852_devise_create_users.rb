c51c946ba227e2400de8dcd54100c81712fe0d07
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-27 19:25:22 +0900

    マイグレーションファイルに追記

--- db/migrate/20210627101852_devise_create_users.rb
+++ db/migrate/20210627101852_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
+      t.text   :occupation,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
