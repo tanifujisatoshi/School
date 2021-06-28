9a5a6b76ad5cf463deec787c02b76bad8dbc1d8f
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-28 21:38:28 +0900

    ログイン状態で表示が変わるように条件分岐をした

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,4 +1,13 @@
 class LunchesController < ApplicationController
   def index
+    
   end
+
+  def destroy
+    lunch = Pr.find(params[:id])
+     if prototype.destroy
+       redirect_to root_path
+     end
+   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'lunches/index'
+  resources :users, only: :show
 end
