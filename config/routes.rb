eec7f6e4fc8a6fe86a9d01e856117c98d1f72304
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-28 22:06:11 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'lunches/index'
+  root to: "lunches#index"
+  resources :lunches  do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
