37f99a829b45ef6567749e8b3af1f9a92283433a
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-06-28 22:22:43 +0900

    マイグレーションファイルに記述

--- db/migrate/20210628131043_create_lunches.rb
+++ db/migrate/20210628131043_create_lunches.rb
@@ -1,7 +1,14 @@
 class CreateLunches < ActiveRecord::Migration[6.0]
   def change
     create_table :lunches do |t|
-
+      t.text :soup,        null: false
+      t.text :staple_food, null: false
+      t.text :main_dish,   null: false
+      t.text :side_dish,   null: false
+      t.text :drink,       null: false
+      t.text :other,       null: false
+      t.date :menu_date,   null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
