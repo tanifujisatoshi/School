790de9c8908bf073f46c825248b2db67149899d5
tanifujisatoshi <83891088+tanifujisatoshi@users.noreply.github.com>
2021-07-03 14:02:19 +0900

    commentモデルにアソシエーションを設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :lunch
+  belongs_to :user
+  validates :text, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_112223) do
+ActiveRecord::Schema.define(version: 2021_07_03_045607) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2021_06_29_112223) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "text"
+    t.bigint "user_id"
+    t.bigint "lunch_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lunch_id"], name: "index_comments_on_lunch_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "lunches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -62,5 +72,7 @@ ActiveRecord::Schema.define(version: 2021_06_29_112223) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "lunches"
+  add_foreign_key "comments", "users"
   add_foreign_key "lunches", "users"
 end
